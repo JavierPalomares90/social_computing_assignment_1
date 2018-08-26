6e8027b2de5ce9b65509f9b41687e56df44aee53
Javier Palomares <javier.palomares.90@gmail.com>
2018-08-26 15:53:44 -0500

    Added code to read input file in DGS.java

--- src/main/java/KM/KM.java
+++ src/main/java/KM/KM.java
@@ -4,6 +4,16 @@ public class KM
 {
     public static void main(String[] args)
     {
+        System.out.println(args.length);
+
+        if(args.length < 1)
+        {
+            System.out.println("Usage: KM [input.txt]");
+            return;
+        }
+        String inputFile = args[0];
+        System.out.println((inputFile));
+
     }
 
 }
