12ca496f34babf1cef0461659e8693f151cf7b34
Javier Palomares <javier.palomares.90@gmail.com>
2018-08-26 17:14:52 -0500

    Added implemenation of DGS algorithm

--- src/main/java/DGS/DGS.java
+++ src/main/java/DGS/DGS.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,6 +25,72 @@ public class DGS
             System.err.println("Input file is malformed");
         }
         //TODO: Run DGS on weights
+        DGS(weights);
+    }
+
+    private static void DGS(int[][] weights)
+    {
+        int numGoods = weights[0].length;
+        int numBidders = weights.length;
+        // The prices and owner of each item
+        double[] prices = new double[numGoods];
+        // TODO: init this to -1
+        int[] owners = new int[numGoods];
+        // Initially all goods are unassigned
+        for (int j = 0; j < owners.length; j++)
+        {
+            owners[j] = -1;
+        }
+        // TODO: Check if this is the correct expression for delta
+        double delta = 1.0 / (numGoods + 1);
+        Queue<Integer> q = new LinkedList<Integer>();
+        // add all bidders to the queue
+        for (int i = 0; i < numBidders; i++)
+        {
+            q.add(i);
+        }
+
+        while(q.isEmpty() == false)
+        {
+            int i = q.poll();
+            double max = -1.0 * Double.MAX_VALUE;
+            int item = 0;
+            // Find the item j that maximizes w_i_j - p_j
+            for (int j = 0; j < numGoods; j++)
+            {
+                double price = prices[j];
+                double tmp = 1.0 * weights[i][j] - price;
+                if(tmp > max)
+                {
+                    max = tmp;
+                    item = j;
+                }
+            }
+            if (max > 0)
+            {
+                int currOwner = owners[item];
+                owners[item] = i;
+                if(currOwner != -1)
+                {
+                    q.add(currOwner);
+                }
+                prices[item] += delta;
+            }
+        }
+
+        int matchingWeight = 0;
+        // print out the owner of item j, and how much they value the item by
+        for (int j = 0;j < owners.length; j++)
+        {
+            int i = owners[j];
+            System.out.println("(" + (i+1) + "," + (j+1) + ")");
+            matchingWeight += weights[i][j];
+        }
+
+        // TODO: Print this before the matches
+        System.out.println(matchingWeight);
+
+
     }
 
     private static int getFirstInteger(String line)
