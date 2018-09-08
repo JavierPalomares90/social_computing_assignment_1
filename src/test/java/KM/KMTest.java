a2639af80290f0ecab181d407e185f17c2c79d5b
Porter Perry <porterperry@Porters-MacBook-Pro.local>
2018-09-07 21:24:05 -0500

    Added assertion of individual matches against true answers for test cases 'test3' and 'test4'.

--- src/test/java/KM/KMTest.java
+++ src/test/java/KM/KMTest.java
@@ -79,9 +79,35 @@ public class KMTest
         assertTrue(matches.length == 30);
         int weight = KM.getMatchingWeight(weights,matches);
         assertTrue(weight == 17889);
-        /**
-         * TODO: assert individual elements
-         */
+        assertTrue(matches[0] == 10);
+        assertTrue(matches[1] == 19);
+        assertTrue(matches[2] == 25);
+        assertTrue(matches[3] == 11);
+        assertTrue(matches[4] == 8);
+        assertTrue(matches[5] == 24);
+        assertTrue(matches[6] == 26);
+        assertTrue(matches[7] == 22);
+        assertTrue(matches[8] == 6);
+        assertTrue(matches[9] == 13);
+        assertTrue(matches[10] == 29);
+        assertTrue(matches[11] == 2);
+        assertTrue(matches[12] == 17);
+        assertTrue(matches[13] == 16);
+        assertTrue(matches[14] == 3);
+        assertTrue(matches[15] == 18);
+        assertTrue(matches[16] == 12);
+        assertTrue(matches[17] == 0);
+        assertTrue(matches[18] == 5);
+        assertTrue(matches[19] == 14);
+        assertTrue(matches[20] == 23);
+        assertTrue(matches[22] == 28);
+        assertTrue(matches[23] == 21);
+        assertTrue(matches[24] == 1);
+        assertTrue(matches[25] == 4);
+        assertTrue(matches[26] == 9);
+        assertTrue(matches[27] == 7);
+        assertTrue(matches[28] == 15);
+        assertTrue(matches[29] == 27);
     }
 
     @Test
@@ -95,9 +121,56 @@ public class KMTest
         assertTrue(matches.length == 50);
         int weight = KM.getMatchingWeight(weights,matches);
         assertTrue(weight == 473);
-        /**
-         * TODO: assert individual elements
-         */
+        assertTrue(matches[0] == 36);
+        assertTrue(matches[1] == 2);
+        assertTrue(matches[2] == 47);
+        assertTrue(matches[3] == 29);
+        assertTrue(matches[4] == 46);
+        assertTrue(matches[5] == 18);
+        assertTrue(matches[6] == 31);
+        assertTrue(matches[7] == 23);
+        assertTrue(matches[8] == 8);
+        assertTrue(matches[9] == 13);
+        assertTrue(matches[10] == 25);
+        assertTrue(matches[11] == 43);
+        assertTrue(matches[12] == 49);
+        assertTrue(matches[13] == 1);
+        assertTrue(matches[14] == 10);
+        assertTrue(matches[15] == 7);
+        assertTrue(matches[16] == 6);
+        assertTrue(matches[17] == 40);
+        assertTrue(matches[18] == 44);
+        assertTrue(matches[19] == 5);
+        assertTrue(matches[20] == 27);
+        assertTrue(matches[21] == 16);
+        assertTrue(matches[22] == 0);
+        assertTrue(matches[23] == 3);
+        assertTrue(matches[24] == 17);
+        assertTrue(matches[25] == 35);
+        assertTrue(matches[26] == 39);
+        assertTrue(matches[27] == 38);
+        assertTrue(matches[28] == 19);
+        assertTrue(matches[29] == 22);
+        assertTrue(matches[30] == 15);
+        assertTrue(matches[31] == 45);
+        assertTrue(matches[32] == 42);
+        assertTrue(matches[33] == 20);
+        assertTrue(matches[34] == 26);
+        assertTrue(matches[35] == 9);
+        assertTrue(matches[36] == 14);
+        assertTrue(matches[37] == 30);
+        assertTrue(matches[38] == 48);
+        assertTrue(matches[39] == 34);
+        assertTrue(matches[40] == 37);
+        assertTrue(matches[41] == 33);
+        assertTrue(matches[42] == 4);
+        assertTrue(matches[43] == 28);
+        assertTrue(matches[44] == 21);
+        assertTrue(matches[45] == 41);
+        assertTrue(matches[46] == 32);
+        assertTrue(matches[47] == 12);
+        assertTrue(matches[48] == 24);
+        assertTrue(matches[49] == 11);
     }
 
     @Test
